119f93ba1fe061d09527c2c273bb9835f92a08b9
KimetsuNoYaiba <djdayunan@gmail.com>
2025-02-24 03:21:35 +0800

    fixing error

--- spawner.js
+++ spawner.js
@@ -3,7 +3,7 @@ global.requireEsm = async function (url) {
 };
 
 global.discordJS = require("discord.js");
-global.telegramJS = require("telegram.js");
+// global.telegramJS = require("telegram.js");
 
 const originalExt = { ...require.extensions };
 const { createRequire } = require("module");
